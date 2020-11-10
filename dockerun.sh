343ce8bee870fb5b5531f84f9fd47838f8d76759
wangmengqiang <wangmengqiang001@126.com>
2020-11-10 15:15:36 +0800

    authenticated by cas
    Signed-off-by: wangmengqiang <wangmengqiang001@126.com>

---
+++ dockerun.sh
@@ -0,0 +1,1 @@
+ docker run -it --name hellospring --add-host sso.isc.com:139.9.236.166 -p 8280:8080 -v $PWD/target/web-java-spring-1.0-SNAPSHOT.war:/usr/local/tomcat/webapps/hellospring.war tomcat:8.5.59-jdk8-openjdk-slim 
